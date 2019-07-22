26b8104442888fd29c049f9134ed6ae2bb5ce8ba
Chae Nicholson <chae.nicholson@gmail.com>
2019-07-22 16:49:07 +0100

    Final version of calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,25 +1,22 @@
 public class Calculator {
-    private double number1;
-    private double number2;
 
-    public Calculator(double number1, double number2) {
-        this.number1 = number1;
-        this.number2 = number2;
+    public Calculator() {
+
     }
 
-    public double addTwoNumbers() {
-        return this.number1 + this.number2;
+    public double add(double number1, double number2) {
+        return number1 + number2;
     }
 
-    public double subtractANumber() {
-        return this.number1 - this.number2;
+    public double subtract(double number1, double number2) {
+        return number1 - number2;
     }
 
-    public double multiplyTwoNumbers() {
-        return this.number1 * this.number2;
+    public double multiply(double number1, double number2) {
+        return number1 * number2;
     }
 
-    public double divideNumber1ByNumber2() {
-        return this.number1 / this.number2;
+    public double divide(double number1, double number2) {
+        return number1 / number2;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,31 +5,34 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-    private Calculator calculator;
-
-    @Before
-    public void before() {
-        calculator = new Calculator( 10.00, 5.00);
-    }
+    private Calculator calculator = new Calculator();
 
     @Test
     public void canAddTwoNumbers() {
-        assertEquals(15.00, calculator.addTwoNumbers(), 0.01);
+        double num1 = 10.00;
+        double num2 = 5.00;
+        assertEquals(15.00, calculator.add(num1, num2), 0.01);
     }
 
     @Test
     public void canSubtractSecondNumberFromFirstNumber() {
-        assertEquals( 5.00, calculator.subtractANumber(), 0.01);
+        double num1 = 22.00;
+        double num2 = 11.00;
+        assertEquals( 11.00, calculator.subtract(num1, num2), 0.01);
     }
 
     @Test
     public void canMultiplyTwoNumbers() {
-        assertEquals(50.00, calculator.multiplyTwoNumbers(), 0.01);
+        double num1 = 4.00;
+        double num2 = 3.00;
+        assertEquals(12.00, calculator.multiply(num1, num2), 0.01);
     }
 
     @Test
     public void canDivideFirstNumberBySecondNumber() {
-        assertEquals( 2.00, calculator.divideNumber1ByNumber2(), 0.01);
+        double num1 = 22.00;
+        double num2 = 11.00;
+        assertEquals( 2.00, calculator.divide(num1, num2), 0.01);
     }
 
 }
